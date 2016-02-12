0e57ecf8244cdc66e2faa26d45be027257263052
Przemyslaw Sowinski <psowinski@users.noreply.github.com>
2016-02-12 14:18:08 +0100

    simple helper to easy my life

---
+++ BooksSimpleGluer/MainForm.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace BooksSimpleGluer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bookText = new System.Windows.Forms.TextBox();
+            this.processButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bookText
+            // 
+            this.bookText.AcceptsReturn = true;
+            this.bookText.AcceptsTab = true;
+            this.bookText.Location = new System.Drawing.Point(12, 12);
+            this.bookText.MaxLength = 0;
+            this.bookText.Multiline = true;
+            this.bookText.Name = "bookText";
+            this.bookText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.bookText.Size = new System.Drawing.Size(758, 502);
+            this.bookText.TabIndex = 0;
+            this.bookText.WordWrap = false;
+            // 
+            // processButton
+            // 
+            this.processButton.Location = new System.Drawing.Point(695, 520);
+            this.processButton.Name = "processButton";
+            this.processButton.Size = new System.Drawing.Size(75, 23);
+            this.processButton.TabIndex = 1;
+            this.processButton.Text = "Process";
+            this.processButton.UseVisualStyleBackColor = true;
+            this.processButton.Click += new System.EventHandler(this.processButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 555);
+            this.Controls.Add(this.processButton);
+            this.Controls.Add(this.bookText);
+            this.Name = "MainForm";
+            this.Text = "Books Simple Gluer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox bookText;
+        private System.Windows.Forms.Button processButton;
+    }
+}
+
---
+++ BooksSimpleGluer/MainForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace BooksSimpleGluer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void processButton_Click(object sender, EventArgs e)
+        {
+            var newLines = new List<string>();
+            var sumLine = "";
+
+            foreach (var line in this.bookText.Lines)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                    continue;
+
+                var trimedLine = line.Trim();
+                if ((!char.IsLetterOrDigit(trimedLine[0]) || char.IsUpper(trimedLine[0])) && sumLine.Length > 10
+                    && sumLine[sumLine.Length - 1] != '-' && !IsContextSpliter(trimedLine))
+                {
+                    newLines.Add(sumLine);
+                    sumLine = trimedLine;
+                }
+                else if (IsContextSpliter(trimedLine))
+                {
+                    if (sumLine.Length > 0)
+                    {
+                        newLines.Add(sumLine);
+                        sumLine = "";
+                    }
+                    newLines.Add("");
+                    newLines.Add("* * *");
+                    newLines.Add("");
+                }
+                else
+                {
+                    if (sumLine.Length > 0 && sumLine[sumLine.Length - 1] == '-')
+                        sumLine += ' ';
+                    sumLine += trimedLine;
+                }
+            }
+            if(sumLine.Length > 0)
+                newLines.Add(sumLine);
+
+            this.bookText.Lines = newLines.ToArray();
+            this.bookText.SelectionStart = 0;
+            this.bookText.SelectionLength = this.bookText.TextLength;
+            this.bookText.Focus();
+        }
+
+        private bool IsContextSpliter(string trimedLine)
+        {
+            return trimedLine == "* * *" || trimedLine == "***";
+        }
+    }
+}
